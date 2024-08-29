aa5fa441cd5a6d6866ab0a477b65f79da4f26d2b
Billingjatt4 <106004217+Billingjatt4@users.noreply.github.com>
2024-08-29 18:23:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Development(Config):
     APP_ID = 25742938
     API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
     # the name to display in your alive message
-    ALIVE_NAME = "LegendBoy"
+    ALIVE_NAME = "♡_🫧𝆺꯭𝅥˶֟፝͟͝β𝝰꯭‌𝞉 ꯭𝝡꯭𝞄꯭𝞌𝞉꯭𝝺꯭𝆺꯭𝅥🍷┼❤️༆"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://u448pcqc011m07:p414ea202b847b0700fe84c34c3354f02857f0d7c2577ca5f99889fe7fdc8e28d@ca75ohcr08rhfe.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dcabus26roofh4"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
